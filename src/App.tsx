88ec20d973b604b21bf7b147e7b807baab444de6
mohammadshughri <mohammadws19@gmail.com>
2022-10-19 11:47:51 +0300

    Added styling + wrong answer logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,34 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-
   const [color, setColor] = useState("");
   const [answers, setAnswers] = useState<string[]>([]);
-
+  const [isWrongSelection, setIsWrongSelection] = useState(false);
 
   const getRandomColor = () => {
-    const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+    const digits = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+    ];
 
-
-    const color = new Array(6).fill("").map(() => digits[Math.floor(Math.random() * digits.length)]).join("");
+    const color = new Array(6)
+      .fill("")
+      .map(() => digits[Math.floor(Math.random() * digits.length)])
+      .join("");
 
     return `#${color}`;
   };
@@ -20,16 +38,33 @@ function App() {
   useEffect(() => {
     const actualColor = getRandomColor();
     setColor(getRandomColor());
-    setAnswers([actualColor, getRandomColor(), getRandomColor()].sort(() => 0.5 - Math.random()))
+    setAnswers(
+      [actualColor, getRandomColor(), getRandomColor()].sort(
+        () => 0.5 - Math.random()
+      )
+    );
   }, []);
+
+  function handleAnswerClicked(answer: string) {
+    if (answer === color) {
+      setIsWrongSelection(false);
+    } else {
+      setIsWrongSelection(true);
+    }
+  }
 
   return (
     <div className="App">
-      <div className="guess-me" style={{ background: color }}></div>
+      <div>
+        <div className="guess-me" style={{ background: color }}></div>
 
-      {answers.map(answer => (
-        <button key={answer}>{answer}</button>
-      ))}
+        {answers.map((answer) => (
+          <button onClick={() => handleAnswerClicked(answer)} key={answer}>
+            {answer}
+          </button>
+        ))}
+        {isWrongSelection && <div className="wrong">Wrong answer!</div>}
+      </div>
     </div>
   );
 }
